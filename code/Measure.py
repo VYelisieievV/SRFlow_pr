b488eade2919adfc14bee390736c604f23614ae3
Vladyslav <46056964+VYelisieievV@users.noreply.github.com>
2023-09-13 11:40:18 +0300

    Update Measure.py

--- code/Measure.py
+++ code/Measure.py
@@ -56,7 +56,7 @@ class Measure():
         return psnr_val
 
     def brisque(self, imgA, imgB):
-        return self.brisque_metric.get_score(imgA)
+        return self.brisque_metric.score(imgA)
 
 
 def t(img):
